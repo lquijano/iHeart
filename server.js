cc195bad3f95ef59b635908b651bd71c6bd851f1
Leslie Quijano <38147387+lquijano@users.noreply.github.com>
2018-10-29 19:44:41 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,41 +1,61 @@
-// Dependencies requirements
-var express = require("express");
-var app = express();
-
-var passport = require("passport");
-var flash = require("connect-flash");
-var mysql = require("mysql");
-
-var morgan = require("morgan");
-var cookieParser = require("cookie-parser");
-var bodyParser = require("body-parser");
-var session = require("express-session");
-
-var router = express.Router();
-var path = require("path");
-
-app.use(express.static("public"));
-
-var PORT = 3000;
-
-// set up our express application
-app.use(morgan("dev")); // log every request to the console
-app.use(cookieParser()); // read cookies (needed for auth)
-app.use(bodyParser.urlencoded({ extended: true })); // get information from html forms
-
-// set up for ejs templating
-app.set("view engine", "ejs");
-// use res.render to load up an ejs view file
-
-//Routes testing
-// app.get("/", function(req, res) {
-//   res.send("<h1>Welcome to the route path</h1>");
-// });
-
-// Initializes the connection variable to sync with a MySQL database
-// module.exports = connection;
-
-// Listener, starting our server
-app.listen(PORT, function() {
-  console.log("app is listening on: " + PORT);
+	//Dependencies
+	const dotenv = require('dotenv').config()
+	const express = require('express');
+	const { urlencoded } = require('body-parser');
+	const twilio = require('twilio');
+	const cookieParser = require('cookie-parser');
+	
+	//Envoking Express
+	let app = express();
+	
+	app.use(cookieParser('sess'));
+	app.use(express.static(__dirname + '/public'));
+	app.use(urlencoded({ extended: false }));
+	
+	//PORT # 3001
+	const PORT = 3001;
+	
+	let twilioClient = new twilio(accountSid, authToken);
+	const number = '3237150014';
+	
+	//Twilio Message Creator
+	twilioClient.messages.create({
+			to: number,  // Text this number
+			from: process.env.TWILIO_NUMBER, // From a valid Twilio number
+		  body: 'Hello! Welcome to FitHeart Tracker. A friend or family member has added you to their First Response Group. For more information visit https://github.com/lquijano/FitHeartTracker'
+	})
+	.then((message) => console.log(message.sid));
+	
+	// initialize the Fitbit API client
+	const fitbitApiClient = require("fitbit-node");
+	const fitbitClient = new FitbitApiClient({
+		fitbitClientId: "FITBIT_ID",
+		fitbitClientSecret: "FITBIT_SECRET",
+		apiVersion: '1.2' // 1.2 is the default
+	});
+	
+	// redirect the user to the Fitbit authorization page
+	app.get("/authorize", (req, res) => {
+		// request access to the user's heartrate, location, profile
+		res.redirect(fitbitClient.getAuthorizeUrl('heartrate, location, profile information', 'https://fithearttracker.herokuapp.com/home'));
+	});
+	
+	// handle the callback from the Fitbit authorization flow
+	app.get("/callback", (req, res) => {
+		// exchange the authorization code we just received for an access token
+		fitbitClient.getAccessToken(req.query.code, 'https://fithearttracker.herokuapp.com/home').then(result => {
+			// use the access token to fetch the user's profile information
+			fitbitClient.get("/profile.json", result.access_token).then(results => {
+				res.send(results[0]);
+			}).catch(err => {
+				res.status(err.status).send(err);
+			});
+		}).catch(err => {
+			res.status(err.status).send(err);
+		});
+	});
+	
+	// Listen on port 3001
+	app.listen(PORT, function() {
+	console.log('🌎 ==> Now listening on PORT %s! Visit http://localhost:%s in your browser!', PORT, PORT);
 });
