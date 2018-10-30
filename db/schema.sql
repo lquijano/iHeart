a70970b90057ab145c8e07c65440f0140d7b3763
esteban gutierrez <estebangutierrez@estebans-MacBook-Air.local>
2018-10-29 19:56:06 -0700

    procfile and db folder added

---
+++ db/schema.sql
@@ -0,0 +1,37 @@
+DROP DATABASE IF EXISTS fitTracker;
+
+CREATE DATABASE fitTracker;
+
+USE fitTracker;
+
+CREATE TABLE users(
+ id INT NOT NULL AUTO_INCREMENT,
+ name VARCHAR(45) NOT NULL,
+ lastname VARCHAR(45) NOT NULL,
+ cellphone INT NOT NULL,
+ email VARCHAR(45) NOT NULL UNIQUE,
+ PASSWORD VARCHAR(45) NOT NULL,
+ PRIMARY KEY (id)
+);
+
+CREATE TABLE firstResponse(
+ id INT NOT NULL AUTO_INCREMENT,
+ user_id INT NOT NULL,
+ name VARCHAR(45) NOT NULL,
+ lastName VARCHAR(45) NOT NULL,
+ relationship VARCHAR(45) NOT NULL,
+ cellphone INT NOT NULL,
+ PRIMARY KEY (id),
+ FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE heartRate(
+id INT NOT NULL AUTO_INCREMENT,
+heartRateUser_id INT NOT NULL,
+heartRate INT NOT NULL,
+PRIMARY KEY (id),
+FOREIGN KEY (heartRateUser_id) REFERENCES users(id)
+);
+
+
+
