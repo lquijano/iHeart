6dc99eedb34f21e7c733461df5265937ffc35e8c
esteban gutierrez <estebangutierrez@estebans-Air.lan>
2018-11-02 19:30:45 -0700

    resolve db, button link to hr

--- db/schema.sql
+++ db/schema.sql
@@ -4,6 +4,7 @@ CREATE DATABASE fitTracker;
 
 USE fitTracker;
 
+
 CREATE TABLE users(
  id INT NOT NULL AUTO_INCREMENT,
  name VARCHAR(45) NOT NULL,
@@ -11,18 +12,9 @@ CREATE TABLE users(
  cellphone INT NOT NULL,
  email VARCHAR(45) NOT NULL UNIQUE,
  PASSWORD VARCHAR(45) NOT NULL,
+ contact VARCHAR(45) NOT NULL,
+ telephone INT NOT NULL,
  PRIMARY KEY (id)
-);
-
-CREATE TABLE firstResponse(
- id INT NOT NULL AUTO_INCREMENT,
- user_id INT NOT NULL,
- name VARCHAR(45) NOT NULL,
- lastName VARCHAR(45) NOT NULL,
- relationship VARCHAR(45) NOT NULL,
- cellphone INT NOT NULL,
- PRIMARY KEY (id),
- FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
 CREATE TABLE heartRate(
@@ -32,6 +24,32 @@ heartRate INT NOT NULL,
 PRIMARY KEY (id),
 FOREIGN KEY (heartRateUser_id) REFERENCES users(id)
 );
+
+-- CREATE TABLE users(
+--  id INT NOT NULL AUTO_INCREMENT,
+--  name VARCHAR(45) NOT NULL,
+--  lastname VARCHAR(45) NOT NULL,
+--  cellphone INT NOT NULL,
+--  email VARCHAR(45) NOT NULL UNIQUE,
+--  PASSWORD VARCHAR(45) NOT NULL,
+--  PRIMARY KEY (id)
+-- );
+
+-- CREATE TABLE firstResponse(
+--  id INT NOT NULL AUTO_INCREMENT,
+--  user_id  INT NOT NULL,
+--  name VARCHAR(45) NOT NULL,
+--  lastName VARCHAR(45) NOT NULL,
+--  relationship VARCHAR(45) NOT NULL,
+--  cellphone INT NOT NULL,
+--  PRIMARY KEY (id),
+--  FOREIGN KEY (user_id) REFERENCES users(id)
+
+
+
+
+
+
 
 
 
